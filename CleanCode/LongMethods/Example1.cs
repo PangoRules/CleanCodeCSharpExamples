89b1a2148dce8b9332b783058c82cec0270a0ad3
Rafael Bolaños <animasdelmundo2@gmail.com>
2022-05-28 20:18:19 -0500

    Refactoring long methods, good example when compare to old method

--- CleanCode/LongMethods/Example1.cs
+++ CleanCode/LongMethods/Example1.cs
@@ -13,93 +13,139 @@ namespace FooFoo
 {
     public partial class Download : System.Web.UI.Page
     {
+        private readonly DataTableToCsvMapper _memoryFileCreator = new DataTableToCsvMapper();
+        private readonly DataTableConsulter _tableReader = new DataTableConsulter();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            System.IO.MemoryStream ms = CreateMemoryFile();
+            ClearResponse();
 
+            SetCacheability();
+
+            WriteContentToResponse(GetCsv());
+        }
+
+        private byte[] GetCsv()
+        {
+            System.IO.MemoryStream ms = _memoryFileCreator.Map(_tableReader.GetDataTable("tableName"));
             byte[] byteArray = ms.ToArray();
             ms.Flush();
             ms.Close();
+            return byteArray;
+        }
 
-            Response.Clear();
-            Response.ClearContent();
-            Response.ClearHeaders();
-            Response.Cookies.Clear();
-            Response.Cache.SetCacheability(HttpCacheability.Private);
-            Response.CacheControl = "private";
+        private void WriteContentToResponse(byte[] byteArray)
+        {
             Response.Charset = System.Text.UTF8Encoding.UTF8.WebName;
             Response.ContentEncoding = System.Text.UTF8Encoding.UTF8;
-            Response.AppendHeader("Pragma", "cache");
-            Response.AppendHeader("Expires", "60");
             Response.ContentType = "text/comma-separated-values";
             Response.AddHeader("Content-Disposition", "attachment; filename=FooFoo.csv");
             Response.AddHeader("Content-Length", byteArray.Length.ToString());
             Response.BinaryWrite(byteArray);
         }
 
-        public System.IO.MemoryStream CreateMemoryFile()
+        private void SetCacheability()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.Private);
+            Response.CacheControl = "private";
+            Response.AppendHeader("Pragma", "cache");
+            Response.AppendHeader("Expires", "60");
+        }
+
+        private void ClearResponse()
+        {
+            Response.Clear();
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Cookies.Clear();
+        }
+    }
+
+    public class DataTableToCsvMapper
+    {
+        public System.IO.MemoryStream Map(DataTable dataTable)
         {
             MemoryStream ReturnStream = new MemoryStream();
 
-            try
-            {
-                string strConn = ConfigurationManager.ConnectionStrings["FooFooConnectionString"].ToString();
-                SqlConnection conn = new SqlConnection(strConn);
-                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM [FooFoo] ORDER BY id ASC", conn);
-                DataSet ds = new DataSet();
-                da.Fill(ds, "FooFoo");
-                DataTable dt = ds.Tables["FooFoo"];
+            StreamWriter streamWritter = new StreamWriter(ReturnStream);
 
-                //Create a streamwriter to write to the memory stream
-                StreamWriter sw = new StreamWriter(ReturnStream);
+            WriteColumnNames(dataTable, streamWritter);
 
-                int iColCount = dt.Columns.Count;
+            WriteRows(dataTable, streamWritter);
 
-                for (int i = 0; i < iColCount; i++)
-                {
-                    sw.Write(dt.Columns[i]);
-                    if (i < iColCount - 1)
-                    {
-                        sw.Write(",");
-                    }
-                }
-
-                sw.WriteLine();
-                int intRows = dt.Rows.Count;
-
-                // Now write all the rows.
-                foreach (DataRow dr in dt.Rows)
-                {
-                    for (int i = 0; i < iColCount; i++)
-                    {
-
-                        if (!Convert.IsDBNull(dr[i]))
-                        {
-                            string str = String.Format("\"{0:c}\"", dr[i].ToString()).Replace("\r\n", " ");
-                            sw.Write(str);
-                        }
-                        else
-                        {
-                            sw.Write("");
-                        }
-
-                        if (i < iColCount - 1)
-                        {
-                            sw.Write(",");
-                        }
-                    }
-                    sw.WriteLine();
-                }
-
-                sw.Flush();
-                sw.Close();
-            }
-            catch (Exception Ex)
-            {
-                throw Ex;
-            }
+            streamWritter.Flush();
+            streamWritter.Close();
+            
             return ReturnStream;
         }
 
+        private static void WriteRows(DataTable dataTable, StreamWriter streamWritter)
+        {
+            foreach(DataRow row in dataTable.Rows)
+            {
+                WriteRow(dataTable, row, streamWritter);
+                streamWritter.WriteLine();
+            }
+        }
+
+        private static void WriteRow(DataTable dataTable, DataRow row, StreamWriter streamWritter)
+        {
+            for(int index = 0; index < dataTable.Columns.Count; index++)
+            {
+                WriteCell(row, streamWritter, index);
+
+                WriteSeparatorIfRequired(dataTable, streamWritter, index);
+            }
+        }
+
+        private static void WriteSeparatorIfRequired(DataTable dataTable, StreamWriter streamWritter, int index)
+        {
+            if(index < dataTable.Columns.Count - 1)
+            {
+                streamWritter.Write(",");
+            }
+        }
+
+        private static void WriteCell(DataRow row, StreamWriter streamWritter, int index)
+        {
+            if(!Convert.IsDBNull(row[index]))
+            {
+                string str = String.Format("\"{0:c}\"", row[index].ToString()).Replace("\r\n", " ");
+                streamWritter.Write(str);
+            }
+            else
+            {
+                streamWritter.Write("");
+            }
+        }
+
+        private static void WriteColumnNames(DataTable dataTable, StreamWriter streamWritter)
+        {
+            for(int index = 0; index < dataTable.Columns.Count; index++)
+            {
+                streamWritter.Write(dataTable.Columns[index]);
+                if(index < dataTable.Columns.Count - 1)
+                {
+                    streamWritter.Write(",");
+                }
+            }
+            streamWritter.WriteLine();
+        }
+    }
+
+    public class DataTableConsulter
+    {
+        private readonly string _connectionString = "FooFooConnectionString";
+
+        public DataTable GetDataTable(string tableName)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[_connectionString].ToString();
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM ["+tableName+"] ORDER BY id ASC", connection);
+            DataSet dataSet = new DataSet();
+            dataAdapter.Fill(dataSet, tableName);
+            DataTable dataTable = dataSet.Tables[tableName];
+            return dataTable;
+        }
     }
 }
